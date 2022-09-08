b9a70e991247cad0637833e0b4e27fb4da791a01
Darshandessai <113026206+Darshandessai@users.noreply.github.com>
2022-09-08 11:22:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className="App">
+      <h1>WELCOME TO HOME PAGE</h1>
+       <h3>This is the Home Page</h3>
+       <h6> THANK-
+        YOU VISIT AGAIN</h6>
+      
+    </div>
+  )
+}
+
+export default App;
